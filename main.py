2a456245c089eecc797e2f481b7d3cc7b606a618
Geovani <geovanicordeiro13@gmail.com>
2023-09-04 18:12:55 -0300

    first commit python(ex016)

---
+++ main.py
@@ -0,0 +1,8 @@
+from math import trunc
+num=float(input('Digite um número fracionado pra ver a sua versão inteira: '))
+print(f'O valor digitado foi {num} e a sua versão inteira é {trunc(num)}!')
+num=float(input('Digite um valor fracionado e receba o valor inteiro: '))
+print(f'O valor fracionado foi {num} e o valor inteiro é {num:.0f}!')
+import math
+num=float(input('Digite um numero inteiro fracionado pra ver a sua versão inteira: '))
+print(f'O valor digitado foi {num} e a sua versão inteira é {math.trunc(num)}: ')
